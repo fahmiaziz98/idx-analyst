2a259bb913c29fb4929ac9dd225e98edfdfbed13
fahmiaziz98 <fahmiazizfadhil09@gmail.com>
2025-10-19 21:57:06 +0700

    init

--- src/test_graph.py
+++ src/test_graph.py
@@ -14,6 +14,8 @@ if __name__ == "__main__":
     #         "What was the total equity of PT BANK RAKYAT INDONESIA (PERSERO) Tbk and its subsidiaries as of December 31, 2024?"
     #     )
     # )
-    asyncio.run(test_graph("Compare revenue bank Jago and BCA in 2024?"))
+    # asyncio.run(test_graph("Compare revenue bank Jago and BCA in 2024?"))
+    # asyncio.run(test_graph("Siapa nama anda?"))
+    asyncio.run(test_graph("Pendapatan PT Astra International"))
     # asyncio.run(test_graph("How does PT BANK JAGO Tbk classify its financial liabilities at initial recognition, and what are the characteristics and accounting treatment of financial liabilities classified as 'held for trading'?"))
     # asyncio.run(test_graph(" What were the details of the share buyback approved by PT Bank Central Asia Tbk's RUPSLB on May 26, 2005?"))
